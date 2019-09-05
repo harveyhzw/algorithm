76a9fda5bfcbac719cdc4b8a9b34ac25728c19d9
harveyhzw <591604023@qq.com>
2019-09-06 04:12:22 +0800

    Add BinaryTree traversal functions

---
+++ src/BinaryTree/BTreeNode.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+public class BTreeNode {
+    public int value;
+    public BTreeNode left;
+    public BTreeNode right;
+    public BTreeNode(int v){
+        this.value=v;
+        this.left=null;
+        this.right=null;
+    }
+}
